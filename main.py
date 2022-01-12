01a6a0b272535655c34c33fb1a6a1b0d56ff04d9
iz0xx <zzowich@gmail.com>
2022-01-12 21:47:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-print("Hello world!;")
-print("Hello;")
-=======
 print("Hello world!;")
 print("Hello;")
 print("Hello my dear friend!;")
 print("Glad to see you!;")
->>>>>>> Feature/hello-advanced
+
